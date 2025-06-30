a80e5f23c78c5b418b875f243f27fd4ddbc8938a
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-06-30 16:01:14 +0100

    Add modal for displaying random quotes and enhance quote management functionality

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,14 +1,24 @@
 const newQuoteText = document.getElementById("newQuoteText");
 const newQuoteCategory = document.getElementById("newQuoteCategory");
-const addQuoteBtn = document.getElementsByClassName("addQuote");
-const showNewQuote = document.getElementById("newQuote");
+const showNewQuoteBtn = document.getElementById("newQuote-btn");
 const quoteList = document.getElementById("quote-list");
+const showModal = document.getElementById("quoteModal");
+const closeModal = document.querySelector(".close-btn");
 
-let quotes = [];
+let quotes = [
+  {
+    text: "Try to always be yourself; everyone is different",
+    category: "Inspiration",
+  },
+  { text: "Keep pushing in every circumstance", category: "Motivation" },
+  { text: "The goal clean and clear of every distraction", category: "Goal" },
+];
 
-function addQuoteToDom(quoteText) {
+function addQuoteToDom(quoteObj) {
   const newQuote = document.createElement("li");
-  const textNode = document.createTextNode(quoteText);
+  const textNode = document.createTextNode(
+    `"${quoteObj.text}" - ${quoteObj.category}`
+  );
   newQuote.appendChild(textNode);
 
   const removeBtn = document.createElement("button");
@@ -17,21 +27,65 @@ function addQuoteToDom(quoteText) {
 
   removeBtn.onclick = function () {
     quoteList.removeChild(newQuote);
-    quotes = quotes.filter((quote) => quote !== quoteText);
+    quotes = quotes.filter(
+      (q) =>
+        !(q.text === quoteObj.text && quoteObj.category === quoteObj.category)
+    );
   };
-  newQuote.appendChild(removeBtn)
+  newQuote.appendChild(removeBtn);
   quoteList.prepend(newQuote);
 }
 
 function addQuote() {
   const quoteText = newQuoteText.value.trim();
+  const quoteCategory = newQuoteCategory.value.trim();
 
-  if (quoteText === "") {
+  if (quoteText === "" || quoteCategory === "") {
     alert("Please enter a quote.");
     return;
   }
+  const quoteObj = { text: quoteText, category: quoteCategory };
 
-  addQuoteToDom(quoteText);
-  quotes.push(quoteText);
+  quotes.push(quoteObj);
+  addQuoteToDom(quoteObj);
+
   newQuoteText.value = "";
+  newQuoteCategory.value = "";
 }
+
+function showRandomQuote() {
+  if (quotes.length === 0) {
+    alert("No quotes available");
+    return;
+  }
+
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomIndex];
+  
+  //set-modal content
+  document.getElementById(
+    "modalQuoteText"
+  ).textContent = `"${randomQuote.text}"`;
+  document.getElementById(
+    "modalQuoteCategory"
+  ).textContent = `-${randomQuote.category}`;
+
+  //show-modal
+  showModal.style.display = "block";
+}
+
+//close model when clicked
+closeModal.onclick = function () {
+  document.getElementById("quoteModal").style.display = "none";
+};
+
+//close modal when clicking outside the modal
+window.onclick = function (event){
+    const modal = showModal;
+    if(event.target === modal){
+        modal.style.display = "none"
+    }
+}
+showNewQuoteBtn.addEventListener("click", showRandomQuote);
+
+// quotes.forEach(addQuoteToDom); //showing default quotes
